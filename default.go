f61a4e204969982c8d3c6f60b5e796ecedeee6c4
qujianping <qjpcpu@gmail.com>
2014-07-12 00:46:49 +0800

    more simple

--- default.go
+++ default.go
@@ -1,16 +1,20 @@
 package logging
 
 import (
+    "fmt"
     stdlog "log"
     "os"
+    "strings"
 )
 
 const (
     default_id = "default-logger"
 )
 
-func InitLogger(level Level) *Logger {
-    default_logger := MustGetLogger(default_id)
+var default_logger *Logger
+
+func InitLogger(level Level) {
+    default_logger = MustGetLogger(default_id)
     format := MustStringFormatter("%{level} %{message}")
     SetFormatter(format)
     // Setup one stdout and one syslog backend.
@@ -24,12 +28,137 @@ func InitLogger(level Level) *Logger {
     // Combine them both into one logging backend.
     SetBackend(logBackend, syslogBackend)
     SetLevel(level, default_id)
-    return default_logger
 }
-func InitSimpleLogger(level Level) *Logger {
-    default_logger := MustGetLogger(default_id)
+func InitSimpleLogger(level Level) {
+    default_logger = MustGetLogger(default_id)
     format := MustStringFormatter("%{level} %{message}")
     SetFormatter(format)
     SetLevel(level, default_id)
-    return default_logger
+}
+
+func SetLogger(logger *Logger) {
+    default_logger = logger
+}
+
+// Fatal is equivalent to l.Critical(fmt.Sprint()) followed by a call to os.Exit(1).
+func Fatal(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    s := strings.TrimRight(fmt.Sprintln(args...), "\n")
+    default_logger.log(CRITICAL, "%s", s)
+    os.Exit(1)
+}
+
+// Fatalf is equivalent to l.Critical followed by a call to os.Exit(1).
+func Fatalf(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(CRITICAL, format, args...)
+    os.Exit(1)
+}
+
+// Panic is equivalent to l.Critical(fmt.Sprint()) followed by a call to panic().
+func Panic(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    s := strings.TrimRight(fmt.Sprintln(args...), "\n")
+    default_logger.log(CRITICAL, "%s", s)
+    panic(s)
+}
+
+// Panicf is equivalent to l.Critical followed by a call to panic().
+func Panicf(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    s := fmt.Sprintf(format, args...)
+    default_logger.log(CRITICAL, "%s", s)
+    panic(s)
+}
+
+// Criticadefault_logger.logs a message using CRITICAL as log level.
+func Critical(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(CRITICAL, "%s", strings.TrimRight(fmt.Sprintln(args...), "\n"))
+}
+func Criticalf(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(CRITICAL, "%s", fmt.Sprintf(format, args...))
+}
+
+// Error logs a message using INFO as log level.
+func Error(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(ERROR, "%s", strings.TrimRight(fmt.Sprintln(args...), "\n"))
+}
+func Errorf(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(ERROR, "%s", fmt.Sprintf(format, args...))
+}
+
+// Warning logs a message using WARNING as log level.
+func Warning(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(WARNING, "%s", strings.TrimRight(fmt.Sprintln(args...), "\n"))
+}
+func Warningf(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(WARNING, "%s", fmt.Sprintf(format, args...))
+}
+
+// Notice logs a message using INFO as log level.
+func Notice(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(NOTICE, "%s", strings.TrimRight(fmt.Sprintln(args...), "\n"))
+}
+func Noticef(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(NOTICE, "%s", fmt.Sprintf(format, args...))
+}
+
+// Info logs a message using INFO as log level.
+func Info(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(INFO, "%s", strings.TrimRight(fmt.Sprintln(args...), "\n"))
+}
+func Infof(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(INFO, "%s", fmt.Sprintf(format, args...))
+}
+
+// Debug logs a message using DEBUG as log level.
+func Debug(args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(DEBUG, "%s", strings.TrimRight(fmt.Sprintln(args...), "\n"))
+}
+func Debugf(format string, args ...interface{}) {
+    if default_logger == nil {
+        InitLogger(DEBUG)
+    }
+    default_logger.log(DEBUG, "%s", fmt.Sprintf(format, args...))
 }
